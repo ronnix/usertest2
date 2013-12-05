9874bf80c16e14c04e408155debe52f035911e3f
Benjamin Pipat <bpipat@gmail.com>
2013-12-05 12:22:56 +0100

    added models to admin.py

---
+++ usertest2/user2/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from models import User, Wine, Movement, Container, Bottle
+
+admin.site.register(User)
+admin.site.register(Wine)
+admin.site.register(Movement)
+admin.site.register(Container)
+admin.site.register(Bottle)
